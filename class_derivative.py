fixed derivatives and added euler method

class_derivative.py
@@ -7,13 +7,13 @@ def class_derivative(**kwargs):
     n = len(y) - 1
 
     #progressive_difference
-    dy = np.arange(0, n+1)
+    dy = []
     i = 0
-    dy[i] = (y[i+1]-y[i])/(x[i+1]-x[i])
+    dy.insert(i, (y[i+1]-y[i])/(x[i+1]-x[i]))
     #central difference
     for j in range(1, n):
-        dy[j] = (y[j+1]-y[j-1])/(x[j+1]-x[j-1])
+        dy.insert(j, (y[j+1]-y[j-1])/(x[j+1]-x[j-1]))
     #regressive difference
-    dy[n] = (y[n]-y[n-1])/(x[n]-x[n-1])
+    dy.insert(n,(y[n]-y[n-1])/(x[n]-x[n-1]))
     
     return dy

class_derivative_2.py
@@ -8,13 +8,13 @@ def first_derivative(**kwargs):
     i = 0
     h = x[i+1]-x[i]
     #progressive_difference
-    dy = np.arange(0, n+1)
-    dy[i] = ((-3*y[i]) + (4*y[i+1]) - y[i+2])/(2*h) #(y[i+1]-y[i])/h
+    dy = []
+    dy.insert(i, ((-3*y[i]) + (4*y[i+1]) - y[i+2])/(2*h)) #(y[i+1]-y[i])/h
     #central difference
     for j in range(1, n-1):
-        dy[j] = (y[j+1]-y[j-1])/(2*h)
+        dy.insert(j, (y[j+1]-y[j-1])/(2*h))
     #regressive difference
-    dy[n] = (y[n-2] - 4*x[n-1] + 3*x[n])/(2*h) #(y[n]-y[n-1])/h
+    dy.insert(n, (y[n-2] - 4*x[n-1] + 3*x[n])/(2*h)) #(y[n]-y[n-1])/h
     
     return dy
 
@@ -26,14 +26,14 @@ def second_derivative(**kwargs):
     i = 0
     h = x[i+1]-x[i]
     #progressive_difference
-    dy = np.arange(0, n+1)  
-    dy[i] = (2*y[i] - 5*y[i+1] + 4*y[i+2] - y[i+3])/(h**2) #(y[i+1]-y[i])/h
+    dy = [] 
+    dy.insert(i, (2*y[i] - 5*y[i+1] + 4*y[i+2] - y[i+3])/(h**2))  #(y[i+1]-y[i])/h
 
     #central difference
     for j in range(1, n):
-        dy[j] = (y[j-1] - 2*y[j] +  y[j+1])/(h**2)
+        dy.insert(j, (y[j-1] - 2*y[j] +  y[j+1])/(h**2))
     #regressive difference
 
-    dy[n] = (-y[n-3] + 4*y[n-2] - 5*y[n-1]+2*y[n])/(h**2)
+    dy.insert(n, (-y[n-3] + 4*y[n-2] - 5*y[n-1]+2*y[n])/(h**2))
     
     return dy